d9e155231818b4ab13aaf2c305e193e0b743f5da
André Schnabel <andreschnabel@me.com>
2017-12-23 20:20:03 +0100

    Bugfixing

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import model
 
+
 def main(args):
     results = model.solve('j3010_1.json')
     print(results)
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- model.py
+++ model.py
@@ -41,27 +41,28 @@ def build_model(p):
         # decision variables
         # primary
         xjt = [[model.addVar(lb=0.0, ub=1.0 if t >= p.efts[j] and t <= p.lfts[j] else 0.0, obj=0.0, vtype=GRB.BINARY,
-                             name=f'x{j},{t}') for t in heurT] for j in J]
+                             name='x' + str(j) + ',' + str(t)) for t in heurT] for j in J]
         # derived
-        zrt = [[model.addVar(lb=0.0, ub=p.zmax[r], vtype=GRB.INTEGER, name=f'z{r},{t}') for t in heurT] for r in R]
+        zrt = [[model.addVar(lb=0.0, ub=p.zmax[r], vtype=GRB.INTEGER, name='z' + str(r) + ',' + str(t)) for t in heurT]
+               for r in R]
 
         # objective
-        revenueForMakespan = quicksum([ p.revenue[t] * xjt[p.numJobs-1][t] for t in time_window(p.numJobs-1)])
-        requiredOvertimeCosts = quicksum([ p.kappa[r] * zrt[r][t] for r in R for t in heurT ])
-        model.setObjective(revenueForMakespan - requiredOvertimeCosts, GRB.MAXIMIZE)
+        revenue_for_makespan = quicksum([p.u[t] * xjt[p.numJobs - 1][t] for t in time_window(p.numJobs - 1)])
+        required_overtime_costs = quicksum([p.kappa[r] * zrt[r][t] for r in R for t in heurT])
+        model.setObjective(revenue_for_makespan - required_overtime_costs, GRB.MAXIMIZE)
 
         # constraints
-        model.addConstrs([quicksum([xjt[j][t] for t in heurT]) == 1 for j in J])
+        model.addConstrs(quicksum([xjt[j][t] for t in heurT]) == 1 for j in J)
 
         def ft(j):
             return quicksum([xjt[j][t] * t for t in time_window(j)])
 
-        model.addConstrs([ft(i) <= ft(j) - p.durations[j] for i in J for j in J if p.adjMx[i][j] == 1 ])
+        model.addConstrs(ft(i) <= ft(j) - p.durations[j] for i in J for j in J if p.adjMx[i][j] == 1)
 
         def cumDemand(r, t):
-            return quicksum([p.demands[j][r] * xjt[j][tau] for j in J for tau in time_window_for_demand(j, t) ])
+            return quicksum([p.demands[j][r] * xjt[j][tau] for j in J for tau in time_window_for_demand(j, t)])
 
-        model.addConstrs([cumDemand(r,t) <= p.capacities[r] + zrt[r][t] for r in R for t in heurT ])
+        model.addConstrs(cumDemand(r, t) <= p.capacities[r] + zrt[r][t] for r in R for t in heurT)
 
         return {'model': model, 'xjt': xjt, 'zrt': zrt}
 
@@ -73,15 +74,18 @@ def solve(instance_filename):
     p = load_project(instance_filename)
     model_compound = build_model(p)
 
-    model, xjt, zrt = [ model_compound[key] for key in ['model', 'xjt', 'zrt'] ]
+    model, xjt, zrt = [model_compound[key] for key in ['model', 'xjt', 'zrt']]
 
     model.update()
     model.optimize()
 
-    xjt_results = [[ xjt[j][t].x  for t in range(p.heuristicMaxMakespan) ] for j in range(p.numJobs) ]
-    zrt_results = [[ zrt[r][t] for r in range(p.numRes) ] for t in range(p.heuristicMaxMakespan)]
-    obj = model.getObjective()
+    xjt_results = [[xjt[j][t].x for t in range(p.heuristicMaxMakespan)] for j in range(p.numJobs)]
+    zrt_results = [[zrt[r][t] for r in range(p.numRes)] for t in range(p.heuristicMaxMakespan)]
+    
+    def st(j):
+        return next(t for t, xjt_val in enumerate(xjt_results[j]) if xjt_results[j][t] > 0.0)
 
-    return { 'xjt': xjt_results, 'zrt': zrt_results, 'obj': obj }
+    starting_times = [st(j) for j in range(p.numJobs)]
+    obj = st(p.numJobs - 1)
 
-
+    return {'Sj': starting_times, 'obj': obj}
